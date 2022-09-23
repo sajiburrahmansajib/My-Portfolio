ccede9fd234896e21e22dc9878d4f5afcd075780
sajiburrahman <mrahman172152@bscse.uiu.ac.bd>
2022-09-23 22:12:02 +0600

    Try to responsive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
-const menuHandaler = () => {
-    const menuDiv = document.getElementById('iconMenu');
-    menuDiv.style.display = 'inline';
-}
+// const menuHandler = () => {
+//     const naMenu = document.getElementById('navMenu');
+//     console.log(naMenu)
+//     naMenu.style.display = 'flex'
+// }
+let count = 0;
+document.getElementById('iconMenu').addEventListener('click', function () {
+    count = count + 1;
+    console.log('click')
+    const naMenu = document.getElementById('navMenu');
+    if (count % 2 === 0) {
+        naMenu.style.display = 'none'
+    }
+    else {
+        naMenu.style.display = 'flex'
+    }
+    console.log(count)
+})
